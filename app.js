cead991d84d12d27b97cd9e98d317b3f9cf616f6
Francisco Magelo <francisco.filho1197@aluno.ce.gov.br>
2025-05-04 20:59:30 -0300

    criação do quiz

--- app.js
+++ app.js
@@ -128,18 +128,18 @@ app.post('/logar-professor', async (req, res) => {
         if (!email || !senha) {
             res.status(400).send("Preencha todos os campos!")
         } else {
-                const professor = await pool.query("SELECT * FROM professores WHERE email = $1", [email]);
-                const result = professor.rows[0];
+            const professor = await pool.query("SELECT * FROM professores WHERE email = $1", [email]);
+            const result = professor.rows[0];
 
-                if (result) {
-                    if (senha === result.senha) {
-                        res.redirect('/');
-                    } else {
-                        res.status(400).send("Senha incorreta!");
-                    }
+            if (result) {
+                if (senha === result.senha) {
+                    res.redirect('/');
                 } else {
-                    res.status(400).send("Professor não encontrado!");
-                
+                    res.status(400).send("Senha incorreta!");
+                }
+            } else {
+                res.status(400).send("Professor não encontrado!");
+
             }
         }
     } catch (error) {
@@ -229,10 +229,105 @@ app.get('/deletar-professores/:id', async (req, res) => {
     }
 });
 
+//Endpoints dos quizes
+
+
+app.get('/quizes/:id', async (req, res) => {
+    const Id = req.params.id;
+    try {
+        if(!Id){
+            res.status(400).send("ID não encontrado");
+        }else{
+        const result = await pool.query('SELECT * FROM quizes where id = $1', [Id]);
+        const quiz = result.rows[0];
+        res.render('pages/quiz');
+    }
+    } catch (error) {
+        console.error("Erro no postgreSQL", error);
+        res.status(400).send("Nenhum quiz encontrado");
+    }
+});
+
+app.post('/inserir-quiz', async (req, res) => {
+    console.log(req.body);
+    const {email,titulo,descricao} = req.body;
+    try {
+        if (!titulo || !descricao || !email) {
+            res.status(400).send("Preencha todos os campos!");
+        } else {
+            const result = await pool.query('SELECT id FROM professores WHERE email = $1', [email]);
+            const Id = result.rows[0].id;
+            await pool.query('INSERT INTO quizes (titulo, descricao, professor_id) VALUES($1, $2, $3)', [titulo,descricao,Id]);
+            res.redirect('/');
+            console.log("Quiz criado com sucesso!");
+        }
+    } catch (error) {
+        console.error("Erro no postgreSQL", error);
+        res.status(400).send("Não foi possível criar o quiz");
+    }
+});
+
+app.get('/editar-quiz/:id', async (req, res) => {
+    const Id = req.paramns.id;
+    try {
+        if (!Id) {
+            res.status(400).send("ID não encontrado!");
+        } else {
+            const result = await pool.query('SELECT * FROM quizes WHERE id = $1', [Id]);
+            res.render('pages/editar-quiz', { quiz: result.rows[0], title: 'Editar Quiz' });
+        }
+    } catch (eror) {
+        console.error("Erro no postgreSQL", error);
+        res.status(400).send("Quiz não encontrado");
+    }
+});
+
+app.post('/atualizar-quiz/:id', async (req, res) => {
+    const Id = req.params.id;
+    const { titulo, descricao } = req.body;
+    try {
+        if (!titulo || !descricao) {
+            res.status(400).send("Preencha todos os campos!");
+        } else {
+            await pool.query('UPDATE quizes SET titulo = $1, descricao = $2 where id = $3', [titulo, descricao, Id]);
+            res.redirect('/users');
+            console.log("Quiz atualizado com sucesso");
+        }
+    } catch (error) {
+        console.error("Erro no postgreSQl", error);
+        res.status(400).send("Não foi possível atualizaar o quiz");
+    }
+});
+
+app.get('/deletar-quizes/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        if (!id) {
+            res.status(400).send("ID não encontrado!");
+        } else {
+            await pool.query('DELETE FROM quizes WHERE id = $1', [id]);
+            res.redirect('/users');
+            console.log("Quiz deletado com sucesso!");
+        }
+    } catch (error) {
+        console.error('Erro no postgreSQL', error);
+        res.status(500).send("Error ao deletar quiz");
+    }
+});
+
+
 //Rotas
 
-app.get('/', (req, res) => {
-    res.render('pages/index', { title: "Home" });
+app.get('/', async (req, res) => {
+    try {
+    const result = await pool.query('SELECT * FROM quizes');
+    const quizes = result.rows;
+    res.render('pages/index', { quizes, title: "Users" });
+
+} catch (error) {
+    console.error('Erro no postgreSQL', error);
+    res.status(500).send("Nenhum dado encontrado");
+}
 });
 
 app.get('/explorar', (req, res) => {
@@ -261,7 +356,7 @@ app.get('/users', async (req, res) => {
 
     } catch (error) {
         console.error('Erro no postgreSQL', error);
-        res.status(500).send("Error ao buscar alunos");
+        res.status(500).send("Nenhum dado encontrado");
     }
 });
 
@@ -272,6 +367,11 @@ app.get('/login-aluno', (req, res) => {
 app.get('/login-professor', (req, res) => {
     res.render('pages/login-professor', { title: "Login do Professor" });
 });
+
+app.get('/criar-quiz', (req,res)=>{
+    res.render('pages/criar-quiz', {title:"Criar Quiz"});
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta:`, PORT);
